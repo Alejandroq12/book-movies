624b4b36034c4fdc63e08c06c756a4f900f78da3
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-04-22 13:15:05 -0600

    Calculate selected seats and total price

--- script.js
+++ script.js
@@ -6,11 +6,21 @@ const movieSelect = document.getElementById('movie');
 
 const ticketPrice = +movieSelect.value;
 
+// Update total and count
+function updateSelectedCount() {
+    const selectedSeats = document.querySelectorAll('.row .seat.selected');
+const selectedSeatsCount = selectedSeats.length;
+count.innerText = selectedSeatsCount;
+total.innerText = selectedSeatsCount * ticketPrice;
+}
+
 container.addEventListener('click', (e) => {
   if (
     e.target.classList.contains('seat') &&
     !e.target.classList.contains('occupied')
   ) {
+    e.target.classList.toggle('selected');
+
+    updateSelectedCount();
   }
-  e.target.classList.toggle('selected');
 });
