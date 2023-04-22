de3d79a5280760dea4ab9cc97ece80337120d47a
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-04-22 15:12:53 -0600

    Set items with local storage

--- script.js
+++ script.js
@@ -6,18 +6,29 @@ const movieSelect = document.getElementById('movie');
 
 let ticketPrice = +movieSelect.value;
 
+//Save selected movie index and price
+function setMovieData(movieIndex, moviePrice) {
+  localStorage.setItem('selectedMovieIndex', movieIndex);
+  localStorage.setItem('selectedMoviePrice', moviePrice);
+}
+
 // Update total and count
 function updateSelectedCount() {
-    const selectedSeats = document.querySelectorAll('.row .seat.selected');
-const selectedSeatsCount = selectedSeats.length;
-count.innerText = selectedSeatsCount;
-total.innerText = selectedSeatsCount * ticketPrice;
+  const selectedSeats = document.querySelectorAll('.row .seat.selected');
+
+  const seatsIndex = [...selectedSeats].map((seat) => [...seats].indexOf(seat));
+
+  localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
+  const selectedSeatsCount = selectedSeats.length;
+  count.innerText = selectedSeatsCount;
+  total.innerText = selectedSeatsCount * ticketPrice;
 }
 
 // Movie select event
-movieSelect.addEventListener('change', e => {
-    ticketPrice = +e.target.value;
-    updateSelectedCount();
+movieSelect.addEventListener('change', (e) => {
+  ticketPrice = +e.target.value;
+  setMovieData(e.target.selectedIndex, e.target.value);
+  updateSelectedCount();
 });
 
 // Seat click event
