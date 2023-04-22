f07c8ac579a95eb0cca8a4361d48bdb9782c7d1e
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-04-22 12:25:04 -0600

    Select elements from the DOM

---
+++ script.js
@@ -0,0 +1,5 @@
+const container = document.querySerlector('.container');
+const seats = document.querySelectorAll('.row .seat:not(.occupied)');
+const count = document.getElementById('count');
+const total = document.getElementById('total');
+const movieSelect = document.getElementById('movie');
