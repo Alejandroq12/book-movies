81d3002876e8225d6bbf4379257a00b26ecf076a
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-04-22 12:38:13 -0600

    Add event listener

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-const container = document.querySerlector('.container');
+const container = document.querySelector('.container');
 const seats = document.querySelectorAll('.row .seat:not(.occupied)');
 const count = document.getElementById('count');
 const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
+
+const ticketPrice = +movieSelect.value;
+
+container.addEventListener('click', (e) =>{
+    if(e.target.classList.contains('seat')) {
+        console.log(e.target);
+    }
+});
