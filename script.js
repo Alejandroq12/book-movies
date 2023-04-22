d0b202170fef726ac186bc969af4a37da88664d4
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-04-22 13:23:40 -0600

    Change the total price according to the current selected mivie

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const count = document.getElementById('count');
 const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
 
-const ticketPrice = +movieSelect.value;
+let ticketPrice = +movieSelect.value;
 
 // Update total and count
 function updateSelectedCount() {
@@ -14,6 +14,13 @@ count.innerText = selectedSeatsCount;
 total.innerText = selectedSeatsCount * ticketPrice;
 }
 
+// Movie select event
+movieSelect.addEventListener('change', e => {
+    ticketPrice = +e.target.value;
+    updateSelectedCount();
+});
+
+// Seat click event
 container.addEventListener('click', (e) => {
   if (
     e.target.classList.contains('seat') &&
